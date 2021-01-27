1951c826901779d07cb4bbff7e742f017ce212ce
a-parcero <aurelie.parcero@gmail.com>
2021-01-27 14:54:11 +0100

    test traitement requete avec zuul

---
+++ zuul-server/src/main/java/simone/zuulserver/filters/LogFilter.java
@@ -0,0 +1,40 @@
+package simone.zuulserver.filters;
+
+import com.netflix.zuul.ZuulFilter;
+import com.netflix.zuul.context.RequestContext;
+import com.netflix.zuul.exception.ZuulException;
+import org.apache.http.protocol.RequestContent;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.http.HttpServletRequest;
+
+@Component
+public class LogFilter extends ZuulFilter {
+
+    Logger log = LoggerFactory.getLogger(this.getClass());
+
+    @Override
+    public String filterType() {
+        return "pre";
+    }
+
+    @Override
+    public int filterOrder() {
+        return 1;
+    }
+
+    @Override
+    public boolean shouldFilter() {
+        return true;
+    }
+
+    @Override
+    public Object run() throws ZuulException {
+
+        HttpServletRequest request = RequestContext.getCurrentContext().getRequest();
+        log.info("******* Requête interceptée!" + request.getRequestURL() + "**************");
+        return null;
+    }
+}
